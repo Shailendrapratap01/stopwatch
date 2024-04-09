b704e3cba1c218d2e69f3d7cfe42a6ff02522087
shailendra <shailendrapratapsingh@innotechteam.in>
2024-04-09 17:12:54 +0530

    stop watch created

--- script.js
+++ script.js
@@ -1,53 +1,115 @@
-const start = document.getElementById("start-btn");
-const reset = document.getElementById("reset-btn");
+const startBtn = document.getElementById("start-btn");
+const resetBtn = document.getElementById("reset-btn");
 const timer = document.getElementById("timer");
-const lap = document.getElementById("lap-btn");
+const lapBtn = document.getElementById("lap-btn");
+const stopBtn = document.getElementById("stop-btn");
+const lapContainer = document.getElementById("lap-container");
 
+let timeInterval;
+let startTime;
+let elapsedTime = 0;
+let lapCnt=0;
+let lapStartTime=0;
 
-let interval;
-let hr = 0;
-let min= 0;
-let sec= 0;
-let millisec=0;
-start.addEventListener("click", ()=>{
-    interval = setInterval(startTimer, 10);
-})
+startBtn.addEventListener("click", () => {
+    startTime = Date.now() - elapsedTime;
+    timeInterval = setInterval(startTimer, 10);
+    stopBtn.style.display = "block"
+    resetBtn.style.display = "block";
+    lapBtn.style.display = "block";
+    startBtn.style.display = "none";
+    lapStartTime = startTime;
+});
 
-reset.addEventListener("click", ()=>{
-    clearInterval(interval);
-    sec=0;
-    min=0;
-    hr=0;
-    millisec=0;
-    timer.innerText = "00:00:00:00";
-})
-let totalLapTime = 0;
-lap.addEventListener("click", ()=>{
-    const lapTime = timer.innerText;
-    totalLapTime
-    console.log(lapTime)
-    console.log(totalLapTime)
-})
+resetBtn.addEventListener("click", () => {
+    clearInterval(timeInterval);
+    startTime=0;
+    elapsedTime=0;
+    timer.innerText = "00:00:00:00:00"
+    stopBtn.style.display = "none";
+    lapBtn.style.display = "none";
+    resetBtn.style.display = "none";
+    startBtn.style.display = "block";
+    lapContainer.innerText ="";
+    lapContainerHeading.style.display = "none";
+    lapCnt=0;
+})  
 
-const startTimer = () =>{
-    millisec++;
-    timer.innerText = `0${hr}:0${min}:0${sec}:${millisec}`;
-    // if(millisec>9){
-    //     timer.innerText = `0${hr}:0${min}:0${sec}:${millisec}`;
-    // }
-    if(sec>9){
-        timer.innerText = `0${hr}:0${min}:${sec}:${millisec}`;
-    }
-    // if(min>9){
-    //     timer.innerText = `0${hr}:${min}:0${sec}:0${millisec}`;
-    // }
-    if(millisec>=99){
-        sec++;
-        millisec=0;
-    }
-    if(sec>=59){
-        min++;
-        sec=0;
-        millisec=0;
-    }
+function startTimer() {
+    elapsedTime = Date.now() - startTime;
+    timer.innerText = toTimeFormate(elapsedTime);
 }
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timeInterval);
+    resetBtn.style.display = "block";
+    startBtn.style.display = "block";
+    stopBtn.style.display = "none";
+    lapBtn.style.display = "none";
+})  
+
+const toTimeFormate = (elapsedTime) => {
+    const date = new Date(elapsedTime);
+    let day = date.getDate()-1;
+    let hr = date.getUTCHours();
+    let min = date.getUTCMonth();
+    let sec = date.getUTCSeconds();
+    let millisec = Math.floor(date.getMilliseconds() / 10);
+
+    let exactDay = day.toString().padStart(2, "0");
+    let exactHour = hr.toString().padStart(2, "0");
+    let exactMin = min.toString().padStart(2, "0");
+    let exactSec = sec.toString().padStart(2, "0");
+    let exactmilli = millisec.toString().padStart(2, "0");
+
+    return `${exactDay}:${exactHour}:${exactMin}:${exactSec}:${exactmilli}`;
+};
+
+lapBtn.addEventListener('click', ()=>{
+    let lapTime = getLapTime();
+    console.log(lapTime)
+    if(lapCnt===0){
+        lapContainerHeading = document.createElement("div")
+        const head1 = document.createElement("p")
+        const head2 = document.createElement("p")
+        const head3 = document.createElement("p")
+        head1.innerText = "lap no."
+        head2.innerText = "lap time"
+        head3.innerText = "total time"
+        lapContainerHeading.appendChild(head1)
+        lapContainerHeading.appendChild(head2)
+        lapContainerHeading.appendChild(head3)
+
+        lapContainerHeading.classList.add("lap-container-heading")
+        lapContainerHeading.style.display = "flex"
+        lapContainer.appendChild(lapContainerHeading)
+
+    }
+
+    lapCnt++;
+    let lapDiv = document.createElement("div")
+    lapDiv.classList.add("lap-div")
+
+    let lapNum = document.createElement("p")
+    lapNum.innerText = `lap${lapCnt}`;
+    lapDiv.append(lapNum)
+
+    let singleLapTime = document.createElement("p")
+    singleLapTime.innerText = lapTime
+    lapDiv.append(singleLapTime)
+
+    let totalLapTime = document.createElement("p")
+    totalLapTime.innerText = timer.innerText
+    lapDiv.append(totalLapTime)
+
+    lapContainer.appendChild(lapDiv)
+})
+
+let totalLapElapsedTime = 0;
+//to get the lap time and totel lap time
+const getLapTime = () => {
+    const elapsedLapTime = Date.now() - lapStartTime;
+    const lapTime = toTimeFormate(elapsedLapTime);
+    lapStartTime = Date.now();
+    return lapTime;
+};
