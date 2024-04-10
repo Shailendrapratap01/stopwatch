539fa783130bab13cc811b248deab6c07c5de6a4
shailendra <shailendrapratapsingh@innotechteam.in>
2024-04-10 14:57:14 +0530

    optimization

--- script.js
+++ script.js
@@ -4,12 +4,26 @@ const timer = document.getElementById("timer");
 const lapBtn = document.getElementById("lap-btn");
 const stopBtn = document.getElementById("stop-btn");
 const lapContainer = document.getElementById("lap-container");
+const yearDisplay = document.getElementById("year");
+const monthDisplay = document.getElementById("month");
+const dayDisplay = document.getElementById("day");
+const hourDisplay = document.getElementById("hour");
+const minuteDisplay = document.getElementById("minute");
+const secondDisplay = document.getElementById("second");
+const millisecondDisplay = document.getElementById("millisecond");
 
 let timeInterval;
 let startTime;
 let elapsedTime = 0;
 let lapCnt=0;
 let lapStartTime=0;
+let year=0 ;
+let month=0;
+let day =0;
+let hour=0;
+let minute=0; 
+let second=0; 
+let millisecond=0;
 
 startBtn.addEventListener("click", () => {
     startTime = Date.now() - elapsedTime;
@@ -19,25 +33,39 @@ startBtn.addEventListener("click", () => {
     lapBtn.style.display = "block";
     startBtn.style.display = "none";
     lapStartTime = startTime;
+    console.log("hi")
 });
 
 function startTimer() {
     elapsedTime = Date.now() - startTime;
-    timer.innerText = toTimeFormate(elapsedTime);
+    let ansArray = toTimeFormate(elapsedTime);
+    millisecondDisplay.innerText = ansArray[6];
+    secondDisplay.innerText = ansArray[5];
+    minuteDisplay.innerText = ansArray[4];
+    hourDisplay.innerText = ansArray[3];
+    dayDisplay.innerText = ansArray[2];
+    monthDisplay.innerText = ansArray[1];
+    yearDisplay.innerText = ansArray[0];
 }
 
 resetBtn.addEventListener("click", () => {
     clearInterval(timeInterval);
     startTime=0;
     elapsedTime=0;
-    timer.innerText = "00:00:00:00:00:00:00"
+    lapCnt=0;
+    millisecondDisplay.innerText = "00";
+    secondDisplay.innerText = "00";
+    minuteDisplay.innerText ="00";
+    hourDisplay.innerText ="00";
+    dayDisplay.innerText = "00";
+    monthDisplay.innerText = "00";
+    yearDisplay.innerText = "00";
     stopBtn.style.display = "none";
     lapBtn.style.display = "none";
     resetBtn.style.display = "none";
     startBtn.style.display = "block";
     lapContainer.innerText ="";
     lapContainerHeading.style.display = "none";
-    lapCnt=0;
 })  
 
 stopBtn.addEventListener("click", () => {
@@ -50,20 +78,35 @@ stopBtn.addEventListener("click", () => {
 
 const toTimeFormate = (elapsedTime) => {
 
-    let totalYears = elapsedTime / 31104000000;
-    let year = Math.floor(totalYears);
-    let totalMonths = (totalYears - year) * 12;
-    let month = Math.floor(totalMonths % 12);
-    let totalDays = (totalMonths - month) * 30;
-    let day = Math.floor(totalDays % 30);
-    let totalHours = (totalDays - day) * 24;
-    let hour = Math.floor(totalHours % 24);
-    let totalMins = (totalHours - hour) * 60;
-    let minute = Math.floor(totalMins % 60);
-    let totalSec = (totalMins - minute) * 60;
-    let second = Math.floor(totalSec % 60);
-    let totalMilliiSeconds = (totalSec - second) * 100;
-    let millisecond = Math.floor(totalMilliiSeconds % 1000);
+    let totalMilliiSeconds = elapsedTime;
+    millisecond = Math.floor(totalMilliiSeconds % 1000);
+    let totalSec = totalMilliiSeconds / 1000;
+    second = Math.floor(totalSec % 60);
+    if (totalSec == 60)
+    {
+        let totalMins = totalSec / 60;
+        minute = Math.floor(totalMins % 60);
+        if(totalMins > 59)    
+        {
+            let totalHours = totalMins / 60;
+            hour = Math.floor(totalHours % 60);
+            if(totalHours > 23)
+            {
+                let totalDays = totalHours / 24;
+                day = Math.floor(totalDays % 60);
+                if(totalDays > 29)
+                {   
+                    let totalMonths = totalDays / 30;
+                    month = Math.floor(totalMonths % 12);
+                    if(totalMonths > 11)
+                    {
+                        let totalyears = totalMonths / 12;
+                        year = Math.floor(totalyears);
+                    }
+                }
+            }
+        }
+    }
 
     let exactYear = year.toString().padStart(2, "0");
     let exactMonth = month.toString().padStart(2, "0");
@@ -81,11 +124,11 @@ const toTimeFormate = (elapsedTime) => {
     let secondString = second > 1 ? "seconds": "second";
     let millisecondString = millisecond > 1 ? "milliseconds": "millisecond";
 
-    return `${exactYear}${yearString} ${exactMonth}${monthString} ${exactDay}${dayString} ${exactHour}${hourString} ${exactMin}${minuteString} ${exactSec}${secondString} ${exactmilli}${millisecondString} `;
+    return [`${exactYear}${yearString}`, `${exactMonth}${monthString}`, `${exactDay}${dayString}`, `${exactHour}${hourString}`, `${exactMin}${minuteString}`, `${exactSec}${secondString}`, `${exactmilli}${millisecondString}`];
 };
 
 lapBtn.addEventListener('click', ()=>{
-    let lapTime = getLapTime();
+    let { lapTime, totalTime } = getLapTime();
     if(lapCnt===0){
         lapContainerHeading = document.createElement("div")
         const head1 = document.createElement("p")
@@ -101,7 +144,6 @@ lapBtn.addEventListener('click', ()=>{
         lapContainerHeading.classList.add("lap-container-heading")
         lapContainerHeading.style.display = "flex"
         lapContainer.appendChild(lapContainerHeading)
-
     }
 
     lapCnt++;
@@ -117,16 +159,19 @@ lapBtn.addEventListener('click', ()=>{
     lapDiv.append(singleLapTime)
 
     let totalLapTime = document.createElement("p")
-    totalLapTime.innerText = timer.innerText
+    totalLapTime.innerText = totalTime
     lapDiv.append(totalLapTime)
 
     lapContainer.appendChild(lapDiv)
 })
 
 //to get the lap time
+let totalLapElapsedTime = 0;
 const getLapTime = () => {
     const elapsedLapTime = Date.now() - lapStartTime;
-    const lapTime = toTimeFormate(elapsedLapTime);
+    const lapTime = toTimeFormate(elapsedLapTime).join(":");
     lapStartTime = Date.now();
-    return lapTime;
+    totalLapElapsedTime += elapsedLapTime;
+    const totalTime = toTimeFormate(totalLapElapsedTime).join(":");
+    return { lapTime, totalTime };
 };
